c6bf5ea6cf0c04653720f126e80d10d8a9cb536d
hdtisystem <123359175+hdtisystem@users.noreply.github.com>
2024-11-15 10:18:59 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "94728110445"
+const SESSION_ID = "PRABATH-MD~gTkWkAYT#IHi4bMJvMTdss4B_29qJwgOBRn8uJoBpbMK8OnBL4Ng"
 
 
 module.exports = {
